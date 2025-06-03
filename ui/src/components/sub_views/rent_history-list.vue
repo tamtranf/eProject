da8c9fa7d24dada44eb872ae26ff2fbaef44e7a5
tam <tran.van.tam@iss-j.com>
2025-06-03 13:10:41 +0900

    last change

--- ui/src/components/sub_views/rent_history-list.vue
+++ ui/src/components/sub_views/rent_history-list.vue
@@ -1,27 +1,8 @@
 <template>
-<<<<<<< HEAD
-
-  <div :class="[name,'page']">
-    <h1>{{ name }} </h1>
-    <div class="col-12" style="">
-
-      <ag-grid-vue style="width: 100%; height: 325px;"
-        class="ag-theme-blue"
-        :gridOptions="gridOptions"
-        >
-      </ag-grid-vue>
-      <br>
-      <!-- <button style="float:right;" class="btn btn-primary" @click="onAddNew"
-      :disabled="checkACL(userAclAction.ADD,aclRules.DATA_PAGES) === false"
-      >New</button>
-      <button style="float:right;margin-right:10px" class="btn btn-danger"
-      :disabled="deleteDisabled" @click="onDeleteSelected">Delete</button> -->
-=======
   <div :class="[name,'page']">
     <h1>{{ name }} </h1>
     <div class="col-12" style="">
       <ag-grid-vue style="width: 100%; height: 325px;" class="ag-theme-blue" :gridOptions="gridOptions" > </ag-grid-vue>
->>>>>>> e8bf277 (renthistory)
     </div>
   </div>
 </template>
@@ -52,12 +33,9 @@ export default {
   ],
   mixins: [mixinLayoutComponents, agListController, datasource, acl],
   computed: {
-<<<<<<< HEAD
-=======
     carID() {
       return this.car_id;
     },
->>>>>>> e8bf277 (renthistory)
     fieldList() {
       return fields;
     },
@@ -68,10 +46,7 @@ export default {
         _.extend(fields.customer_name, {}),
         _.extend(fields.from_date, {}),
         _.extend(fields.to_date, {}),
-<<<<<<< HEAD
-=======
         _.extend(fields.total_rent_hours, {}),
->>>>>>> e8bf277 (renthistory)
         _.extend(fields.rent_value, {}),
         _.extend(fields.notes, {}),
       ];
