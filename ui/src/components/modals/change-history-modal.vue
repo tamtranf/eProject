85c67c3218152e174d7142c00e455f08d8f85ee2
tranvantamver2 <tran.van.tam@iss-j.com>
2025-06-14 13:55:46 +0900

    tam

--- ui/src/components/modals/change-history-modal.vue
+++ ui/src/components/modals/change-history-modal.vue
@@ -1,7 +1,8 @@
 <template>
+  <button type="button" @click="showModal()" style="float:right;margin-right:10px;" class="btn btn-info">History</button>
 <div
   class="modal fade"
-  ref="confirmationModal"
+  ref="modalChangeHistory"
   id="staticBackdrop"
   data-bs-backdrop="static"
   data-bs-keyboard="false"
@@ -9,11 +10,11 @@
   aria-labelledby="staticBackdropLabel"
   aria-hidden="true"
 >
-   <div class="modal-dialog">
-    <div class="modal-content">
+   <div class="modal-dialog modal-xl" style="height: 95%;">
+    <div class="modal-content" style="height: 95%;">
 
       <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">{{ title }}</h5>
+        <h5 class="modal-title" id="staticBackdropLabel">History</h5>
         <button
           type="button"
           class="btn-close"
@@ -23,13 +24,52 @@
         ></button>
       </div>
 
-      <div class="modal-body">
-        <div class="row">
-            <div class="col-12" v-html="text">
+      <div class="modal-body" style="height: 95%; padding: 0px 15px; overflow-x: auto;">
+  <ag-grid-vue
+    style="width: 100%; height: 125px;"
+    class="ag-theme-blue"
+    :gridOptions="gridOptions">
+  </ag-grid-vue>
 
-            </div>
-        </div>
-      </div>
+  <div class="row">
+    <!-- Bảng dữ liệu A -->
+    <div class="col-6">
+      <table class="hist_td" style="margin: auto; margin-top: 30px; width: 100%;">
+        <tr>
+          <th class="hist_td">項目</th>
+          <th class="hist_td">古いデータ</th>
+          <th class="hist_td">新しいデータ</th>
+        </tr>
+        <tr v-for="d in dataChangesA" :key="d.id">
+          <th class="hist_td">{{ d.label }}</th>
+          <td class="hist_td">{{ d.o }}</td>
+          <td v-if="d.n.length > 0" class="hist_td" style="color: red;">{{ d.n }}</td>
+          <td v-else class="hist_td">{{ d.o }}</td>
+        </tr>
+      </table>
+    </div>
+
+    <!-- Bảng dữ liệu B -->
+    <div class="col-6">
+      <table
+        v-if="Array.isArray(dataChangesB)"
+        class="hist_td"
+        style="margin: auto; margin-top: 30px; width: 100%;">
+        <tr>
+          <th class="hist_td">項目</th>
+          <th class="hist_td">古いデータ</th>
+          <th class="hist_td">新しいデータ</th>
+        </tr>
+        <tr v-for="d in dataChangesB" :key="d.id">
+          <th class="hist_td">{{ d.label }}</th>
+          <td class="hist_td">{{ d.o }}</td>
+          <td v-if="d.n.length > 0" class="hist_td" style="color: red;">{{ d.n }}</td>
+          <td v-else class="hist_td">{{ d.o }}</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</div>
 
       <div class="modal-footer">
         <button type="button" :class="['btn', cancel_class]" @click="onClose('no')"> {{cancel_text}} </button>
@@ -47,6 +87,10 @@ import mixinLayoutComponents from '@/mixins/layout_components';
 import _ from 'lodash';
 import acl from '@/mixins/acl';
 import { Modal } from 'bootstrap';
+import agListController from '@/mixins/ag-list-controller';
+import datasource from '@/mixins/datasource';
+import { fields } from '../../../../api/rules/fields_change_history';
+import change_history from '../../../../api/models/change_history';
 
 export default {
   name: '',
@@ -54,9 +98,11 @@ export default {
     return {
       local_fields_ref_a: false,
       local_fields_ref_b: false,
-      api_name: '',
+      api_name: 'change_history',
+      data_changes: [],
       name: '',
       modalElem: null,
+
       title: 'Modal',
       text: 'Text',
       ok_text: 'OK',
@@ -64,28 +110,92 @@ export default {
       ok_class: 'btn-primary',
       cancel_class: 'btn-secondary',
       on_confirm: () => {},
+
     };
   },
 
-  mixins: [mixinLayoutComponents, mixinFormController, acl],
+  mixins: [mixinLayoutComponents, mixinFormController, acl, agListController, datasource],
   components: {},
   computed: {
+    dataChangesA() {
+      const c = parseInt(this.dataChanges.length / 2) + 1;
+      return _.chunk(this.dataChanges, c)[0];
+    },
+    dataChangesB() {
+      const c = parseInt(this.dataChanges.length / 2) + 1;
+      return _.chunk(this.dataChanges, c)[1];
+    },
+    dataChanges: {
+      get() {
+        return this.data_changes;
+      },
+      set(v) {
+        this.data_changes = v;
+      },
+    },
+    fieldList() {
+      return fields;
+    },
+    tabFieldList() {
+      return [
+        _.extend(fields.change_time, {}),
+        _.extend(fields.user_name, {}),
+        _.extend(fields.mode, {
+          cellRenderer: (params) => {
+            if (params.value === 1) {
+              return '新規';
+            }
+            if (params.value === 2) {
+              return '更新';
+            }
+            if (params.value === 3) {
+              return '消去';
+            }
+            return '-';
+          },
+        }),
+      ];
+    },
 
   },
   methods: {
-    onClose(data) {
+    onCLose() {
       this.modalElem.hide();
-      this.on_confirm(data);
     },
-    showModal(data) {
+    showModal() {
       this.modalElem.show();
-      this.title = data.title;
-      this.text = data.text;
-      this.ok_text = data.ok_text;
-      this.cancel_text = data.cancel_text;
-      this.ok_class = data.ok_class;
-      this.cancel_class = data.cancel_class;
-      this.on_confirm = data.on_confirm;
+      this.gridOptions.api.sizeColumnsToFit();
+      this.gridOptions.api.purgeInfiniteCache();
+      this.countRow('ChangeHistoryModal');
+    },
+    createColumnDefs() {
+      return this.commonCreateColumnDefs({ show_details: false, show_checkbox: false });
+    },
+    onRowSelected(data_obj) {
+      const { data } = data_obj;
+      const use_fields = this.$props.tab_fields;
+      console.log('onRowSelected', { data, use_fields });
+
+      const old_data = JSON.parse(data.old_data);
+      const new_data = JSON.parse(data.new_data);
+
+      const all_keys = Object.keys(old_data);
+
+      Object.keys(new_data).forEach((k) => {
+        if (!all_keys.includes(k)) {
+          all_keys.push(k);
+        }
+      });
+      this.dataChanges = [];
+
+      all_keys.forEach((k) => {
+        if (k.indexOf('seq_id') < 0 && use_fields && use_fields[k]) {
+          const obj = {
+            id: k,
+            label: use_fields[k]?.label || k,
+          };
+        }
+      });
     },
 
   },
@@ -100,7 +210,7 @@ export default {
     console.log(`${this.name} beforeMount`);
   },
   mounted() {
-    this.modalElem = new Modal(this.$refs.confirmationModal);
+    this.modalElem = new Modal(this.$refs.modalChangeHistory);
     console.log(`${this.name} mounted`);
     // if (this.seqId === 'new') {
     this.is_new = true;
